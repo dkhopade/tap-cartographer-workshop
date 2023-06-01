e595a9ed35e3fef204f5b1fc6ca5e7ff06bd2ba7
dkhopade <deepak.khopade@gmail.com>
2023-06-01 09:11:26 -0400

    ch container registry

--- local-config/scripts/imgbuild.sh
+++ local-config/scripts/imgbuild.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-IMAGE_REPO=harbor.services.demo.jg-aws.com
+#IMAGE_REPO=harbor.services.demo.jg-aws.com
+IMAGE_REPO=dktapdemo.azurecr.io
 
 # echo "Reinitialize password manager"
 # pass show docker-credential-helpers/docker-pass-initialized-check
 
 # echo "Docker login"
-# # docker login https://harbor.services.demo.jg-aws.com --username tap-workshop --password ''
+docker login $IMAGE_REPO --username dktapdemo --password 'QMOAadagDR9/u/kLXt/pLWxFbSrOJRbZlnXE+YxU4U+ACRB9CKBD'
 
 echo "Building workshop image"
 docker build . -t $IMAGE_REPO/tap-workshop/cartographer-workshop
